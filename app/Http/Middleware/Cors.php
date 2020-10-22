9dbddcf459f965638bff9f2d348292e9b7d3f3b8
AleCeballos <4lejandroceballos@gmail.com>
2020-10-22 16:31:18 -0300

    CORS 1 OK

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,12 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
 
         //Url a la que se le dará acceso en las peticiones
-      header("Access-Control-Allow-Origin", "http://localhost:4200/")
+      ->header("Access-Control-Allow-Origin", "http://localhost:4200")
       //Métodos que a los que se da acceso
-      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
       //Headers de la petición
       ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     }
