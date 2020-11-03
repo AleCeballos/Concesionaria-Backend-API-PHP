4aaf81c9fa016203b87e91621b7236f986d1a8cd
AleCeballos <4lejandroceballos@gmail.com>
2020-11-03 19:12:07 -0300

    Deploy 5

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,29 @@
-<?php
+    <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
 
 
-Route::group(['middleware' => ['cors']], function () {
-    //Rutas a las que se permitirá acceso
 
-//rutas para el controlador de usuarios
-Route::post('/api/users','UserController@register');
-Route::post('/api/sesions','UserController@login');
 
-//rutas para el controlador de autos
-// controlador restful
-Route::resource('/api/cars','CarController');
-});
+    Route::group(['middleware' => ['cors']], function () {
+        //Rutas a las que se permitirá acceso
+
+     //rutas para el controlador de usuarios
+         Route::post('/api/users','UserController@register');
+         Route::post('/api/sesions','UserController@login');
+         Route::resource('/api/cars','CarController');
+    });
